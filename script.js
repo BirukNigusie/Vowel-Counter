7a14969036805d9c0270171b023fdaea19dbbd16
BirukNigusie98 <biruknigusie98@gmail.com>
2024-09-19 16:44:41 +0300

    Inital commit

--- script.js
+++ script.js
@@ -1,69 +1,18 @@
-const minuteLabel = document.getElementById('minute');
-const secondLabel = document.getElementById('second');
-const millisecondLabel = document.getElementById('millisecond');
-
-const startBtn = document.getElementById('startBtn');
-const stopBtn = document.getElementById('stopBtn');
-const pauseBtn = document.getElementById('pauseBtn');
-const resetBtn = document.getElementById('resetBtn');
-const lapList = document.getElementById('laplist');
-
-let minutes = 0;
-let seconds = 0;
-let milliseconds = 0;
-let interval;
- 
-
-startBtn.addEventListener('click', () => {
-interval = setInterval(updateTimer,10);
-startBtn.disabled = true;
-});
-
-stopBtn.addEventListener('click', () => {
-clearInterval(interval);
-addLapList();
-reset();
-startBtn.disabled = false;
-});
-
-pauseBtn.addEventListener('click', () => {
-clearInterval(interval);
-startBtn.disabled = false;
-});
-
-resetBtn.addEventListener('click',reset );
-
-function addLapList(){
-    const lapTime = `${padValue(minutes)}:${padValue(seconds)}:${padValue(milliseconds)}`;
-    const listItem = document.createElement('li')
-    listItem.innerHTML = `<span>Lap ${lapList.childElementCount + 1}:</span>${lapTime}`;
-    lapList.appendChild(listItem)
+function countVowels(){
+    let text = document.getElementById("inputText").value;
+    let vowelCount = 0;
+    text = text.toLowerCase();
+    for (let i = 0; i < text.length; i++) {
+let char = text.charAt(i);
+if(isVowel(char)){
+    vowelCount++;
 }
- function reset(){
-clearInterval(interval);
-milliseconds = 0;
-seconds = 0;
-minutes = 0;
-displayTimer(); 
-startBtn.disabled = false;
-}
-function updateTimer(){
-    milliseconds++;
-    if(milliseconds === 100){
-        milliseconds = 0;
-        seconds++;
+
     }
-    if(seconds === 60){
-        seconds = 0;
-        minutes++;
-    }
-    displayTimer();
+    const result = document.getElementById("result");
+    result.textContent = `Total Vowels: ${vowelCount}`
 }
-function displayTimer(){
-    millisecondLabel.textContent = padValue(milliseconds); 
-    secondLabel.textContent = padValue(seconds);
-    minuteLabel.textContent = padValue(minutes);
-}
-function padValue(time){
-return time.toString().padStart(2, "0")//add padding of certain value(0) until desiered length(2 digit)
+function isVowel(char){
+const vowels = ['a', 'e', 'i', 'o', 'u'];  
+return vowels.includes(char) 
 }
